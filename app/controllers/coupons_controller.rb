60eeb091fc772aef52e16054305519d125451002
Matthew Jones <matts3371@gmail.com>
2020-04-02 16:02:06 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -12,11 +12,7 @@ class CouponsController < ApplicationController
   end 
   
   def create 
-    @coupon = Coupon.new 
-    @coupon.coupon_code = params[:coupon_code]
-    @coupon.store = params[:store]
-    @coupon.save 
-    
+    Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
     redirect_to "/coupons/#{@coupon.id}"
   end 
 end 
