a54b81286faa422e43a2968f6c95153d5d7b06eb
Matthew Jones <matts3371@gmail.com>
2020-04-02 15:59:30 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :coupons, only: [:index, :show, :create]
+  resources :coupons, only: [:index, :new, :create, :show]
 
 end
