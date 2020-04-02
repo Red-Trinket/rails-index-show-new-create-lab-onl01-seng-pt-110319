0abb5dcd9e5402d5156eef624861d900716c72d8
Matthew Jones <matts3371@gmail.com>
2020-04-02 15:43:03 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,22 @@
+class CouponsController < ApplicationController
+  
+  def index 
+    @coupons = Coupons.all 
+  end 
+  
+  def show 
+    @coupon = Coupons.find(params[:id])
+  end 
+  
+  def new 
+  end 
+  
+  def create 
+    @coupon = Coupon.new 
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save 
+    
+    redirect_to coupons_path(@coupon) 
+  end 
+end 
